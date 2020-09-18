ab4c9dc3fa976c0b47b308fd4f08bad7b81f1694
Amit Cirt <siamitcs0@gmail.com>
2020-09-18 15:44:49 +0300

    Change log attributes name.

--- vigoler/logger.go
+++ vigoler/logger.go
@@ -7,11 +7,11 @@ type Logger struct {
 }
 
 func (l *Logger) startDownloadLive(url VideoUrl, output string) {
-	l.Logger.Info("Start downloading live", zap.Any("url", url), zap.String("output", output))
+	l.Logger.Info("Start downloading live", zap.Any("video_url", url), zap.String("output", output))
 }
 func (l *Logger) liveRecreated(url VideoUrl, output string) {
-	l.Logger.Info("Recreate live to download", zap.Any("url", url), zap.String("output", output))
+	l.Logger.Info("Recreate live to download", zap.Any("video_url", url), zap.String("output", output))
 }
 func (l *Logger) finishDownloadLive(url VideoUrl, output string) {
-	l.Logger.Info("Finish downloading live", zap.Any("url", url), zap.String("output", output))
+	l.Logger.Info("Finish downloading live", zap.Any("video_url", url), zap.String("output", output))
 }
